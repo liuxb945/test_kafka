d7d5e8f90073cdfd81190ba55c5ee2cdd8ec82c3
liuxb945 <280445715@qq.com>
2017-02-02 21:29:03 +0800

    dd

---
+++ src/main/java/com/abcd/test_kafka/MyConsumer.java
@@ -0,0 +1,36 @@
+package com.abcd.test_kafka;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+ 
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+ 
+public class MyConsumer {
+	public static void main(String[] args) {
+		String topic = "lxw1234.com";
+		ConsumerConnector consumer = Consumer.createJavaConsumerConnector(createConsumerConfig()); 
+		Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+		topicCountMap.put(topic, new Integer(1));
+		Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumer.createMessageStreams(topicCountMap);
+		KafkaStream<byte[], byte[]> stream =  consumerMap.get(topic).get(0);
+		ConsumerIterator<byte[], byte[]> it = stream.iterator();
+	    while(it.hasNext())
+	      System.out.println("consume: " + new String(it.next().message()));
+	}
+	
+	private static ConsumerConfig createConsumerConfig() {
+	    Properties props = new Properties();
+	    props.put("group.id","group1");
+	    props.put("zookeeper.connect","192.168.140.128:2181");
+	    props.put("zookeeper.session.timeout.ms", "400");
+	    props.put("zookeeper.sync.time.ms", "200");
+	    props.put("auto.commit.interval.ms", "1000");
+	    return new ConsumerConfig(props);
+	  }
+}
